0f10052391e144d7898a275a641177c36c030bbd
Ekber1235 <ekber160309@gmail.com>
2024-08-31 18:22:50 +0400

    homework

---
+++ app.js
@@ -0,0 +1,54 @@
+let month = prompt("Write the month number")
+switch (month) {
+    case "1":
+    case "01":
+   console.log("January");     
+   break;
+   case "2":
+    case "02":
+   console.log("February");     
+   break;
+   case "3":
+    case "03":
+   console.log("March");     
+   break;
+   case "4":
+    case "04":
+   console.log("April");     
+   break;
+   case "5":
+    case "05":
+   console.log("May");     
+   break;
+   case "6":
+    case "06":
+   console.log("June");     
+   break;
+   case "7":
+    case "07":
+   console.log("July");     
+   break;
+   case "8":
+    case "08":
+   console.log("August");     
+   break;
+   case "9":
+    case "09":
+   console.log("September");     
+   break;
+   case "10":
+    case "10":
+   console.log("October");     
+   break;
+   case "11":
+    case "11":
+   console.log("November");     
+   break;
+   case "12":
+    case "12":
+   console.log("December");     
+   break;
+    default:
+        console.log("There is no such month")
+       
+}
